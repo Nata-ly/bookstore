c1d89f40beeaa913bc2da177d52b72203400e3da
Nikiforova Natalia <nikifor1114@mail.ru>
2021-10-05 22:32:25 +0700

    Add comment model and migrate, book:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_105626) do
+ActiveRecord::Schema.define(version: 2021_10_05_152828) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2021_10_03_105626) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.integer "estimate"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_comments_on_book_id"
   end
 
 end
