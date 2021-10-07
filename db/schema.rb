e50befd44f4744468989371a3da436a60ab7e473
Nikiforova Natalia <nikifor1114@mail.ru>
2021-10-07 10:03:01 +0700

    Generate devise User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_152828) do
+ActiveRecord::Schema.define(version: 2021_10_07_024809) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,18 @@ ActiveRecord::Schema.define(version: 2021_10_05_152828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_comments_on_book_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
