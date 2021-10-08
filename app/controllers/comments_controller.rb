ee2dfe8963e6a8427929821f930de5f4235717dd
Nikiforova Natalia <nikifor1114@mail.ru>
2021-10-08 18:03:20 +0700

    Tied comment with User

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,23 @@ class CommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @book.comments.create(comment_params)
-    redirect_to book_path(@book)
+    comment = @book.comments.new(comment_params)
+    set_user(comment)
+    if comment.save
+      redirect_to book_path(@book)
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :author, :estimate)
+    params.require(:comment).permit(:body, :estimate)
+  end
+
+  def set_user(comment)
+    comment.author = current_user.username
+    comment.user_id = current_user.id
+    comment
   end
 
 end
