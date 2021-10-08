542df8550c83fc2339a9fd075900da4c8c1dc049
Nikiforova Natalia <nikifor1114@mail.ru>
2021-10-08 15:52:21 +0700

    Add migrate refences user to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_060950) do
+ActiveRecord::Schema.define(version: 2021_10_08_075119) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -30,7 +30,9 @@ ActiveRecord::Schema.define(version: 2021_10_07_060950) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["book_id"], name: "index_comments_on_book_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
