3787c6f92b366d4dce0350c24f6e6bbc9db38c13
Nikiforova Natalia <nikifor1114@mail.ru>
2021-10-12 22:24:04 +0700

    Add category model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_145924) do
+ActiveRecord::Schema.define(version: 2021_10_12_151906) do
 
   create_table "authors", force: :cascade do |t|
     t.string "author"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2021_10_12_145924) do
     t.decimal "price"
     t.string "picture_url"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
