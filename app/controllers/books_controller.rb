69be91d38966de28dda1e335f2b1c38079ecce4e
Nikiforova Natalia <nikifor1114@mail.ru>
2021-10-12 18:59:22 +0700

    Added search by author

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,13 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all
+    @books = Book
+    @books = @books.where('author LIKE ?', "\%#{params[:search_author]}\%") if params[:search_author]
+    @books = @books.order(:id)
   end
 
   def show
     @book=Book.find(params[:id])
   end
-
+  
 end
