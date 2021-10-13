7bda936308d9b19a65c01effcbcd818efb0c3dbf
Nikiforova Natalia <nikifor1114@mail.ru>
2021-10-13 12:35:20 +0700

    Fixed bug Book model renamed belongs_to

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   has_many :comments
-  belongs_to :authors
-  belongs_to :categories
-  validates :title, :author, :price, presence: true
+  belongs_to :author
+  belongs_to :category
+  validates :title, :price, presence: true
   validates :price, length: { minimum: 2 }
 end
