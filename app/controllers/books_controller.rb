8a77f1f9ab2873b8b23111a0a91d8f2285532b93
Nikiforova Natalia <nikifor1114@mail.ru>
2021-10-13 20:52:36 +0700

    Added search by category

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   def index
     @books = Book
     @books = @books.where(author_id: params[:author]) if params[:author]
+    @books = @books.where(category_id: params[:category]) if params[:category]
     @books = @books.order(:id)
   end
 
