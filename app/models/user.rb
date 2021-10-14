a34ebe4a6f5ce2161912786ff52ea0fa9984469b
Nikiforova Natalia <nikifor1114@mail.ru>
2021-10-14 22:23:24 +0700

    Add before_validation :normalize_username to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_validation :normalize_username, on: :create
+
+  private
+
+  def normalize_username
+    self.username = username.downcase.titleize
+  end
+
 end
