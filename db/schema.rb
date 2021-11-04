decb5743eb2a5b62d16392ef12bab2fca3bb5130
Nata-lu <nikifor1114@mail.ru>
2021-11-04 18:54:07 +0700

    Add manager model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_082219) do
+ActiveRecord::Schema.define(version: 2021_11_04_114804) do
 
   create_table "authors", force: :cascade do |t|
     t.string "author"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 2021_10_28_082219) do
     t.integer "user_id"
     t.index ["book_id"], name: "index_comments_on_book_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "managers", force: :cascade do |t|
+    t.string "name"
+    t.string "lastname"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
